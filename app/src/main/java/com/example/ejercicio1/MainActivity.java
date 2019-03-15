315edfafc0be0f202ab176a9bc8e7bb7f08fa78e
danielorozco14 <00200617@uca.edu.sv>
2019-03-15 14:30:16 -0600

    Ejercicio 1 Terminado

--- app/src/main/java/com/example/ejercicio1/MainActivity.java
+++ app/src/main/java/com/example/ejercicio1/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.ejercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public Button boton1;
+    public EditText cajaUserName, cajaPassword;
+    public TextView Texto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        boton1=findViewById(R.id.boton_click);
+        cajaUserName=findViewById(R.id.user_inputText);
+        cajaPassword=findViewById(R.id.password_inputText);
+        Texto=findViewById(R.id.show_text);
+
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Texto.setText(cajaUserName.getText());
+            }
+        });
+
+        boton1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                Texto.setText(cajaPassword.getText());
+                return true;
+            }
+        });
+
+
+
+
+
+
     }
+
+
+
+
 }
